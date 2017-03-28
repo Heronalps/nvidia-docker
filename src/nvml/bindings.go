326c097937c44e3d60f86a6fd5b8f2554235e54c
cmdhema <cmdhema@gmail.com>
2017-03-28 17:58:46 +0900

    Edit LDFLAGS, CFLAGS

--- src/nvml/bindings.go
+++ src/nvml/bindings.go
@@ -2,7 +2,8 @@
 
 package nvml
 
-// #cgo LDFLAGS: -ldl -Wl,--unresolved-symbols=ignore-in-object-files
+//#cgo CFLAGS: -I /usr/local/cuda-8.0/include -I /usr/include/nvidia-361/cuda/cuda
+//#cgo LDFLAGS: -L /usr/local/cuda-8.0/lib64 -ldl -Wl,--unresolved-symbols=ignore-in-object-files
 // #include "nvml_dl.h"
 import "C"
 
